af0b7f5038d40b6bca5765c88d2e76befbb1d532
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2026-02-11 03:25:18 -0600

    Add Raylib

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-
+#include "raylib.h"
 
 int main() {
-	printf("CHIPPIE is back!\n");
+	int res_mul = 20;
+	Color off_color = {0x92, 0x68, 0x1A, 0xFF};
+	Color on_color = {0xF5, 0xCE, 0x3A, 0xFF};
+	InitWindow(64 * res_mul, 32 * res_mul, "CHIPPIE");
+
+	while(!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(off_color);
+		DrawText("CHIPPIE is back!", 0, 0, 50, on_color);
+		EndDrawing();
+	}
 }
