e6aeda521e270419c32dd3718ef7a10c5591a70e
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2026-02-13 08:36:19 -0600

    Update utils.c
    Rearranging includes fixed getBit not being declared
    Fixed getBit itself as well

--- src/utils.c
+++ src/utils.c
@@ -1,14 +1,13 @@
+#include "utils.h"
+#include <math.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <math.h>
-#include "utils.h"
-
 
 bool getBit(uint8_t byte, uint8_t bit)
 {
 	// Generate a mask
-	uint8_t mask = (uint8_t) pow(2, bit) >> (uint8_t) pow(2, bit - 1);
+	uint8_t mask = (uint8_t) pow(2, bit);
 	
 	// Return masked and shifted value
-	return (byte & mask) >> bit;
+	return (byte & mask) >> ( bit - 1 );
 }
