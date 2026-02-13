33afbee8f41d98f973e7e344cd804fa7e8696a76
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2026-02-13 04:02:05 -0600

    Update CHIP-8 Structure

--- src/CHIP-8/chip8.h
+++ src/CHIP-8/chip8.h
@@ -4,11 +4,11 @@
 
 // Struct representation of a CHIP-8 machine
 typedef struct _Chip8 {
-	unsigned char ram[4096];     // 4 KB of memory
-	bool display[64 * 32];       // 64 x 32 monochrome display
-	unsigned int pc;             // Program counter
-	uint16_t stack[100];         // Stack (limited; unsure what a good cap would be)
-	unsigned char delay_timer;   // Delay Timer
-	unsigned char sound_timer;   // Sound Timer
-	unsigned char registers[16]; // All 16 variable registers
+	uint8_t  ram[4096];        // 4 KB of memory
+	bool     display[64 * 32]; // 64 x 32 monochrome display
+	uint16_t pc;               // Program counter
+	uint16_t stack[100];       // Stack (limited; unsure what a good cap would be)
+	uint8_t  delay_timer;      // Delay Timer
+	uint8_t  sound_timer;      // Sound Timer
+	uint8_t  registers[16];    // All 16 variable registers
 } Chip8;
