7f74cda962fad2d0cb419dae747cd9df367436e8
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2026-02-13 04:05:45 -0600

    Add index register
    I forgot it, whoops

--- src/CHIP-8/chip8.h
+++ src/CHIP-8/chip8.h
@@ -7,6 +7,7 @@ typedef struct _Chip8 {
 	uint8_t  ram[4096];        // 4 KB of memory
 	bool     display[64 * 32]; // 64 x 32 monochrome display
 	uint16_t pc;               // Program counter
+	uint16_t i;                // Index register
 	uint16_t stack[100];       // Stack (limited; unsure what a good cap would be)
 	uint8_t  delay_timer;      // Delay Timer
 	uint8_t  sound_timer;      // Sound Timer
