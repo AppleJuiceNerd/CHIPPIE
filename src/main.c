7ec3f42eb914c1589c0836f80ee88bfd81274102
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2026-02-18 16:44:55 -0600

    Limit framerate
    Limiting framerate limits cycle speed for now

--- src/main.c
+++ src/main.c
@@ -22,6 +22,7 @@ int main(int argc, char** argv) {
 	Color off_color = {0x92, 0x68, 0x1A, 0xFF};
 	Color on_color = {0xF5, 0xCE, 0x3A, 0xFF};
 	InitWindow(64 * res_mul, 32 * res_mul, "CHIPPIE");
+	SetTargetFPS(700);
 
 
 	while(!WindowShouldClose())
